00aa584bc1e1800cdd3732fcbe01cb7f5eb2e730
Rian Corcino <riancorci@gmail.com>
2024-02-24 11:23:00 -0800

    added 3x tap upgrade

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -2,24 +2,54 @@ package com.example.tapcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private var counter: Int = 0
+    private var multiplier: Int = 1
+
+    private lateinit var tap: Button
+    private lateinit var count: TextView
+    private lateinit var upgrades: Button
+    private lateinit var flash: ImageView
+    private lateinit var quote: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         counter = 0
 
-        val tap = findViewById<Button>(R.id.tap)
-        val count = findViewById<TextView>(R.id.counter)
+        tap = findViewById(R.id.tap)
+        count = findViewById(R.id.counter)
+        flash = findViewById(R.id.flash)
+        quote = findViewById(R.id.quote)
 
         tap.setOnClickListener {
-            counter++
+            counter+=multiplier
             count.setText(counter.toString())
+
+            if (counter == 20) {
+                setUpgrades()
+            }
+        }
+    }
+
+    private fun setUpgrades() {
+        upgrades = findViewById(R.id.upgrade)
+        upgrades.visibility = View.VISIBLE
+
+        upgrades.setOnClickListener {
+            upgrades.isEnabled = false
+            multiplier = 3
+
+            quote.visibility = View.VISIBLE
+
+            flash.setImageResource(R.drawable.flash_run)
         }
     }
 }

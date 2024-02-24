ac1985863d82fd98fe1a194b8aa2ebbbd2983c07
Rian Corcino <riancorci@gmail.com>
2024-02-24 10:52:06 -0800

    finished counter game

--- app/src/main/java/com/example/tapcounter/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.tapcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counter = 0
+
+        val tap = findViewById<Button>(R.id.tap)
+        val count = findViewById<TextView>(R.id.counter)
+
+        tap.setOnClickListener {
+            counter++
+            count.setText(counter.toString())
+        }
     }
 }
